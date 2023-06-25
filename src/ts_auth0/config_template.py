aa0020c2be597fc68edcadd0cb7145104b555b4d
Jon Richards <jrseti@gmail.com>
2023-06-24 21:45:54 -0700

    Removed config_template.py

--- src/ts_auth0/config_template.py
+++
@@ -1,7 +0,0 @@
-config = {
-            "API_KEY" : "Your API key",  
-            "API_SECRET_KEY" : "Your secret API key",
-            "AUTHORIZATION_REDIRECT_PORT" : 3000,
-            "AUTHORIZATION_REDIRECT_URI" : "http://localhost",
-            "AUTHORIZATION_SCOPE" : "openid profile MarketData ReadAccount Trade Matrix Crypto OptionSpreads offline_access",
-}
